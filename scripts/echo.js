66d52bdf41d4303356af0000023590db436caba4
Meera Sundar <mirage.sundial@gmail.com>
2020-09-13 14:17:41 +0530

    clean up trailing whitespace logic

--- scripts/echo.js
+++ scripts/echo.js
@@ -2,8 +2,11 @@
 const fs = require('fs')
 let outputString = ''
 process.argv.forEach((val, index) => {
-    if(index > 1){
+    if((index > 1) && (index < process.argv.length - 1)){
         outputString += val + ' '
+    }
+    else if(index === process.argv.length-1){
+        outputString += val
     }
 })
 console.log(outputString)
